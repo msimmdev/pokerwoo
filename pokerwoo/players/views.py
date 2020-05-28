fccd3017d4e928e52edbce68777061e7bcec70e0
Michael Simm <michael@simm.dev>
2020-05-28 19:50:03 +0100

    Add method to retrieve detail of logged in player.

--- pokerwoo/players/views.py
+++ pokerwoo/players/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, views, response, status
 from players.models import Player, PaymentObligation
 from players.serializers import PlayerSerializer, PaymentObligationSerializer, UserSerializer
 from django.contrib.auth.models import User
@@ -17,3 +17,12 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.filter(groups__name='Poker Players')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class ActivePlayer(views.APIView):
+    def get(self, request):
+        players = Player.objects.filter(user=self.request.user.id)
+        if len(players) > 0:
+            serializer = PlayerSerializer(players[0], context={'request': request})
+            return response.Response(serializer.data)
+        else:
+            return response.Response(status=status.HTTP_404_NOT_FOUND)
