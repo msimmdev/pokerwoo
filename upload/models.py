192a0828bd75fee92d1f8e02d3fdf23780aa6527
Michael Simm <michael@simm.dev>
2020-06-21 23:53:27 +0100

    Uploads to different bucket.

--- upload/models.py
+++ upload/models.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 from django.db import models
+from storages.backends.s3boto3 import S3Boto3Storage
 
 # Create your models here.
 
@@ -9,4 +10,5 @@ def upload_path(instance, filename):
     return 'avatars/{0}{1}'.format(str(uuid.uuid4()), file_extension)
 
 class AvatarUpload(models.Model):
-    file = models.FileField(upload_to=upload_path)
+    fs = S3Boto3Storage(bucket='pokerwoo')
+    file = models.FileField(upload_to=upload_path, storage=fs)
