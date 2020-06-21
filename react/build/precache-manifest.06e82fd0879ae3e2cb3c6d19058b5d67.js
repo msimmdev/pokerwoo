86455819c06ea3f7b46fffb66c6df1c8f2374c61
Michael Simm <michael@simm.dev>
2020-06-21 23:24:41 +0100

    Changes for demo envinroment.

---
+++ react/build/precache-manifest.06e82fd0879ae3e2cb3c6d19058b5d67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370fdd3f86a7d1e80edec00bdf9fafa8",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/index.html"
+  },
+  {
+    "revision": "3a444f13e4d165d29619",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/css/main.97072527.chunk.css"
+  },
+  {
+    "revision": "e0a29bf4271c4c536cb3",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/js/2.efba89fe.chunk.js"
+  },
+  {
+    "revision": "a33649f45084893f2f04ac6c58f0cd50",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/js/2.efba89fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a444f13e4d165d29619",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/js/main.febeaa6a.chunk.js"
+  },
+  {
+    "revision": "8064b7269fc29a168d9c",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/js/runtime-main.6d6024ec.js"
+  },
+  {
+    "revision": "b95e5959e6b56d8084d73dfb7f8c3471",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/media/pokerwoo-icon.b95e5959.svg"
+  },
+  {
+    "revision": "947597047cc5a6ca222d546b1ab71161",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/media/pokerwoo.94759704.svg"
+  },
+  {
+    "revision": "a8521e3819c15b24d4ab035d1fb21c5e",
+    "url": "https://pokerwoo-demo.s3.eu-west-2.amazonaws.com/static/media/saarland.a8521e38.ttf"
+  }
+]);
