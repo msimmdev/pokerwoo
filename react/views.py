b9df40b2f449a07b40089ed078f471bbb25178f6
Michael Simm <michael@simm.dev>
2020-06-28 20:12:13 +0100

    Temprarily disable manual approval.

--- react/views.py
+++ react/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render, redirect
 
 def index(request):
     if request.user.is_authenticated:
-        if request.user.groups.filter(name='Poker Players').exists():
-            return render(request, "build/index.html")
-        else:
-            return render(request, 'registration/request_access.html')
+        return render(request, "build/index.html")
+#        if request.user.groups.filter(name='Poker Players').exists():
+#            return render(request, "build/index.html")
+#        else:
+#            return render(request, 'registration/request_access.html')
     else:
         return redirect('/accounts/login/')
